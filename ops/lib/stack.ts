eff081d6cfc62c9c7ab9ad9439162b25daaae2a1
Tom Wright <tom.suffern.wright@gmail.com>
2022-09-06 09:07:21 +1000

    wip: add dynamodb table to cdk

--- ops/lib/stack.ts
+++ ops/lib/stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,5 +12,14 @@ export class Stack extends cdk.Stack {
     const lambda = new DockerImageFunction(this, "Lambda", {
       code,
     });
+
+    const table = new Table(this, "DynamoTable", {
+      tableName: "comment-vibe",
+      partitionKey: { name: "pk", type: AttributeType.STRING },
+      sortKey: { name: "sk", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+
+    table.grantReadWriteData(lambda);
   }
 }
