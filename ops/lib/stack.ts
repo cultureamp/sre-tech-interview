8a99b1f9e2c4d2d296bef40f1098257254cb4a76
Tom Wright <tom.suffern.wright@gmail.com>
2022-09-05 21:58:33 +1000

    wip: add docker image lambda

--- ops/lib/stack.ts
+++ ops/lib/stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const code = DockerImageCode.fromImageAsset(`${__dirname}/../..`);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'OpsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambda = new DockerImageFunction(this, "Lambda", {
+      code,
+    });
   }
 }
