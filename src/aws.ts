3eabbb516c5f6870bc01634d33bbaa9a2b9a3538
Tom Wright <tom.suffern.wright@gmail.com>
2022-09-08 08:35:12 +1000

    wip: fix placeholder credentials

--- src/aws.ts
+++ src/aws.ts
@@ -1,10 +1,12 @@
-export const createAWSOptions = function () {
+export const createAWSOptions = () => {
   if (process.env["IS_LOCAL"] == "true") {
     return {
       region: "local",
-      endpoint: "http://localhost:8000",
-      accessKeyId: "local",
-      secretAccessKey: "local",
+      endpoint: "http://dynamodb:8000",
+      credentials: {
+        accessKeyId: "local",
+        secretAccessKey: "local",
+      },
     };
   } else {
     return {};
