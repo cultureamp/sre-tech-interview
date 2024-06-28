9a7df5a2b2ebbf5bb2cb5b9440dd33ff53095086
James Telfer <792299+jamestelfer@users.noreply.github.com>
2024-06-28 11:47:40 +1000

    fix: make create-table idempotent
    Avoid confusion when the table already exists: an error is expected in this case.

--- dynamodb/create-table.sh
+++ dynamodb/create-table.sh
@@ -4,4 +4,10 @@ export AWS_REGION=local
 export AWS_ACCESS_KEY_ID=local
 export AWS_SECRET_ACCESS_KEY=local
 
-aws dynamodb --endpoint http://dynamodb:8000 create-table --cli-input-json file://create-table.json
+endpoint="http://dynamodb:8000"
+
+if aws dynamodb --endpoint "$endpoint" describe-table --table-name comment-vibe --query 'Table.TableStatus' > /dev/null 2>&1; then
+  echo "Table already exists, no action required. Remove and recreate the 'dynamodb' container to reset the database."
+else
+  aws dynamodb --endpoint "$endpoint" create-table --cli-input-json file://create-table.json
+fi
